cdbfdcf34d03ca34a5cc0f81b65c0a0a3c6a881a
djordjevicivana <djordjevic.it59.2019@uns.ac.rs>
2025-04-06 23:53:34 +0200

    created and added footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,19 +6,24 @@ import Dashboard from "./pages/Dashboard"
 import Articles from "./pages/Articles"
 import About from "./pages/About"
 import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 export default function App() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
+
         <Route path= "/" element={<Home />} />
         <Route path= "/about" element={<About />} />
         <Route path= "/sign-in" element={<SignIn />} />
         <Route path= "/sign-up" element={<SignUp />} />
         <Route path= "/dashboard" element={<Dashboard />} />
         <Route path= "/articles" element={<Articles />} />
+
       </Routes>
+      
+      <Footer />
     </BrowserRouter>
   )
 }
