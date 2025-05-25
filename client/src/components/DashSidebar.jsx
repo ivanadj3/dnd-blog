0d5a92654112824d2d015ee67e8c84d4311b9c13
Ivana Djordjevic <djordjevic-ivana@hotmail.com>
2025-05-25 17:05:03 +0200

    completed user image upload functionality

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -5,7 +5,7 @@ import { Link, useLocation } from 'react-router-dom';
 
 export default function DashSidebar(){
     const location = useLocation()
-    const [tab, setTab] = useState('')
+    const [tab, setTab] = useState('');
     useEffect(() => {
         const urlParams = new URLSearchParams(location.search)
         const tabFromUrl = urlParams.get('tab')
@@ -18,9 +18,9 @@ export default function DashSidebar(){
             <Sidebar.Items>
                 <Sidebar.ItemGroup>
                     <Link to='/dashboard?tab=profile'>
-                    <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={'User'} labelColor='dark'>
-                        Profile
-                    </Sidebar.Item>
+                        <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={'User'} labelColor='dark' as='div'>
+                            Profile
+                        </Sidebar.Item>
                     </Link>
                     <Sidebar.Item icon={HiArrowSmRight} className='cursor-pointer'>
                         Sign Out
