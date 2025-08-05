c3856ece93d492423e2d3b21a24afbd3d7caf9f4
Ivana Djordjevic <djordjevic-ivana@hotmail.com>
2025-08-05 20:23:02 +0200

    added signout functionality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -5,7 +5,7 @@ import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/
 import { app } from '../firebase';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
-import { updateStart, updateSuccess, updateFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess } from '../redux/user/userSlice';
+import { updateStart, updateSuccess, updateFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess, signoutSuccess } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
 import { HiOutlineExclamationCircle } from 'react-icons/hi';
 
@@ -129,11 +129,27 @@ export default function DashProfile(){
         if(!res.ok) {
             dispatch(deleteUserFailure(data.message));
         } else {
-            dispatch(deleteUserFailure(error.message));
+            dispatch(deleteUserSuccess(data));
         }
 
     }catch(error){
         dispatch(deleteUserFailure(error.message));
+    }
+  };
+
+  const handleSignOut = async() => {
+    try {
+        const res = await fetch('/api/user/signout', {
+           method: 'POST', 
+        });
+        const data = await res.json();
+        if(!res.ok) {
+            console.log(data.message)
+        } else {
+            dispatch(signoutSuccess());
+        }
+    } catch (error) {
+        console.log(error.message);
     }
   };
 
@@ -176,7 +192,7 @@ export default function DashProfile(){
             </form>
             <div className='text-red-500 flex justify-between mt-5'>
                 <span onClick={()=>setShowModal(true)} className='cursor-pointer'>Delete Account</span>
-                <span className='cursor-pointer'>Sign Out</span>
+                <span onClick={handleSignOut} className='cursor-pointer'>Sign Out</span>
             </div>
             {updateUserSuccess && (
                 <Alert color='success' className='mt-5'>
