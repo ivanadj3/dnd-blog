effba93839fbe80c346f82a11c496d887a8c1dd0
Ivana Djordjevic <djordjevic-ivana@hotmail.com>
2025-08-06 15:51:54 +0200

    added create a post api route

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,29 +1,32 @@
-import mongoose, { mongo } from "mongoose";
+import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     username: {
-        type: String,
-        required: true,
-        unique: true,
+      type: String,
+      required: true,
+      unique: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true,
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String,
-        required:true,
+      type: String,
+      required: true,
     },
-    profilePicture:{
-        type: String,
-        default: "https://images.icon-icons.com/3158/PNG/512/viking_halloween_costume_character_avatar_icon_193275.png",
+    profilePicture: {
+      type: String,
+      default:
+        'https://images.icon-icons.com/3158/PNG/512/viking_halloween_costume_character_avatar_icon_193275.png',
     },
     isAdmin: {
-        type: Boolean,
-        default: false,
+      type: Boolean,
+      default: false,
     },
-    }, {timestamps: true}
+  },
+  { timestamps: true }
 );
 
 const User = mongoose.model('User', userSchema);
