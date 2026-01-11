fed9bce1b8b67f8ef7a12f6cedcbadb84693ab1d
Ivana Djordjevic <djordjevic-ivana@hotmail.com>
2026-01-11 18:30:14 +0100

    completed about and project pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
-
 export default function About() {
   return (
-    <div>About</div>
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div>
+          <h1 className='text-3xl font font-semibold text-center my-7'>About Ivana's Blog</h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6'>
+            <p>
+              Welcome to a place built for storytellers, worldbuilders, and curious minds. My platform is a community-driven blog and hub where users can share articles, ideas, and insights around the games, worlds, and stories they love. Whether you’re breaking down mechanics, publishing homebrew content, writing lore, or just sharing an opinion, this is a space designed to make creativity easy to publish and fun to explore.
+            </p>
+            <p>
+              Users can interact with each other through comments, likes, and discussions, turning articles into shared inspiration.
+            </p>
+            <p>
+              Whether you’re here to write, read, or just browse, we’re glad you’re part of the journey.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- client/src/pages/Articles.jsx
+++ client/src/pages/Articles.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import CallToAction from '../components/CallToAction';
 
 export default function Articles() {
   return (
-    <div>Articles</div>
+    <div className='min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3'>
+      <h1 className='text-3xl font-semibold'>Articles</h1>
+      <p className='text-md text-gray-500'>A community-driven collection of articles covering ideas, guides, opinions, and creative content. Read, react, and join the conversation.</p>
+      <CallToAction />
+    </div>
   )
 }
